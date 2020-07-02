7faa7fb09476a2dea980ee4aad7e9968faf2c3b0
Huda Prasetyo <huda.prs2002@gmail.com>
2020-07-02 15:28:23 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import Landing from './components/landings/Landing';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Landing />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    background: '#fff',
+  },
+});
+
+export default App;
---
+++ components/landings/Landing.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {View, Text, StyleSheet, Image, ImageBackground} from 'react-native';
+
+const Landing = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../../assets/img/LandingBody.png')}
+        style={styles.image}
+      />
+      <Image
+        source={require('../../assets/img/LandingHeader.png')}
+        style={styles.imageHeader}
+      />
+      <Text style={styles.landingText}>Selamat Datang!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  imageHeader: {
+    position: 'absolute',
+    marginLeft: 35,
+    marginTop: 30,
+    zIndex: 1,
+  },
+  landingText: {
+    fontSize: 16,
+    textAlign: 'center',
+    color: '#407BFF',
+    marginBottom: 50,
+  },
+});
+
+export default Landing;
